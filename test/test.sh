d9267bf9dadc7acf239d33c14537fd30869cd0e5
Dan Osborne <daniel.osborne@metaswitch.com>
2015-07-29 16:21:57 -0700

    fixed path to json file in test.sh

--- test/test.sh
+++ test/test.sh
@@ -4,5 +4,5 @@ docker-compose up -d
 export IP_ADDRESS=`docker inspect --format='{{.NetworkSettings.IPAddress}}' metaswitchmodules_marathon_1`
 # Wait for the marathon container to come up
 sleep 10
-curl -X POST http://$IP_ADDRESS:8080/v2/apps -d @$0/sjc-static.json -H "Content-type: application/json"
+curl -X POST http://$IP_ADDRESS:8080/v2/apps -d @`dirname $0`/sjc-static.json -H "Content-type: application/json"
 docker exec metaswitchmodules_slave1_1 ping -c 4 192.168.0.2
