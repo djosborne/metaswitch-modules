1b111ba0aa3460e8652f45e595a9462755777efd
Dan Osborne <daniel.osborne@metaswitch.com>
2015-07-30 10:41:58 -0700

    increased sleep time to see if delay is causing failure

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,6 @@ docker-compose up -d
 
 export IP_ADDRESS=`docker inspect --format='{{.NetworkSettings.IPAddress}}' metaswitchmodules_marathon_1`
 # Wait for the marathon container to come up
-sleep 15
+sleep 100
 curl -X POST http://$IP_ADDRESS:8080/v2/apps -d @`dirname $0`/sjc-static.json -H "Content-type: application/json"
 docker exec metaswitchmodules_slave1_1 ping -c 4 192.168.0.2
