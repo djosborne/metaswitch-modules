0e9d84af88ade95e390c9f3fac9a7ebfbda7bf91
Dan Osborne <daniel.osborne@metaswitch.com>
2015-07-31 16:46:15 -0700

    testing jenkins auto build

--- test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker-compose up -d
-
-export IP_ADDRESS=`docker inspect --format='{{.NetworkSettings.IPAddress}}' metaswitchmodules_marathon_1`
-# Wait for the marathon container to come up
-sleep 100
-curl -X POST http://$IP_ADDRESS:8080/v2/apps -d @`dirname $0`/sjc-static.json -H "Content-type: application/json"
-docker exec metaswitchmodules_slave1_1 ping -c 4 192.168.0.2
